0fc0ac691e999781f72ed40a76e5dec11d6d3290
Khuleg <26bs38301nest.edu.mn@G301-CC0210.local>
2026-04-09 14:23:22 +0800

    push

---
+++ app/components/Upcoming.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Upcoming = () => {
+  return (
+    <div className="bg-[url('/images/movie01.jpg')] bg-cover bg-no-repeat h-screen w-full bg-center"></div>
+  );
+};
